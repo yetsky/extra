d9e1d6f1b8fd658cbbba1765b3439deaf04014b5
yetsky <yetsky@gmail.com>
2015-01-23 21:01:00 +0800

    bug

--- luci/applications/luci-chinadns/luasrc/controller/chinadns.lua
+++ luci/applications/luci-chinadns/luasrc/controller/chinadns.lua
@@ -1,22 +1,23 @@
 --[[
-RA-MOD
+QQ:76888484
+http://yetsky.taobao.com/
 ]]--
 
 module("luci.controller.chinadns", package.seeall)
 
 function index()
-	
+
 	if not nixio.fs.access("/etc/config/chinadns") then
 		return
 	end
-
+	
 	local page
-	page = node("admin", "RA-MOD")
+	page = node("admin", "gfw")
 	page.target = firstchild()
-	page.title = _("RA-MOD")
-	page.order  = 65
+	page.title = _("梯子")
+	page.order  = 64
 
-	page = entry({"admin", "RA-MOD", "chinadns"}, cbi("chinadns"), _("chinadns"), 55)
+	page = entry({"admin", "gfw", "chinadns"}, cbi("chinadns"), _("chinadns"), 5)
 	page.i18n = "chinadns"
 	page.dependent = true
 end
