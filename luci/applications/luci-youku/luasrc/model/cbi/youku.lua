589922beaad1f0d28b472161716a635743dc2dca
yetsky <yetsky@gmail.com>
2015-05-23 14:11:19 +0800

    new

--- luci/applications/luci-youku/luasrc/model/cbi/youku.lua
+++ luci/applications/luci-youku/luasrc/model/cbi/youku.lua
@@ -13,41 +13,44 @@ if running  then
 m = Map("youku", translate("优酷路由宝"), "<p style='text-align:left'>"..translate("路由宝正在工作中....").."<br></br>"..translate("WAN口速率 ")..sudu.."</p>")
 else
 if run then
-m = Map("youku", translate("优酷路由宝"), translate("路由宝准备工作在...."))
+m = Map("youku", translate("优酷路由宝"), translate("路由宝正在启动中...."))
 else
 m = Map("youku", translate("优酷路由宝"), translate("路由宝已停止工作...."))
 end
 end
 s = m:section(TypedSection, "youku", translate("路由宝<a href=\"http://yjb.youku.com\" target=\"_blank\">  点击进入官方金币平台>></a>"))
 s.anonymous = true
-o = s:option(Flag, "enable", translate("是否启用矿机"))
-o = s:option(ListValue, "oksn", translate("SN途径"))
-o:value("1", translate("路由宝原版SN"))
-o = s:option(DummyValue,"","<p style='text-align:left'><strong>"..translate("序列号sn: ").."</strong>".."<font color='green'> <strong>" ..yksn.."</strong><br></br></font><strong>"..translate("一键绑定: ").."</strong>".."<font color='green'> <strong>" ..bd_button.."</strong><br></br></font><strong>"..translate("绑定状态: ").."</strong>".."<font color='green'> <strong>" ..bdzt.."</strong></font></p>")
-o:depends({oksn="1"})
 
-o = s:option(Value, "opsn", translate("原版SN"))
-o:depends({oksn="1"})
-o = s:option(ListValue, "wkmod", translate("挖矿模式"))
+--o = s:option(ListValue, "oksn", translate("SN途径"))
+--o:value("1", translate("路由宝原版SN"))
+o = s:option(DummyValue,"","<p style='text-align:left'><strong>"..translate("序列号sn: ").."</strong>".."<font color='green'> <strong>" ..yksn.."</strong><br></br></font><strong>"..translate("一键绑定: ").."</strong>".."<font color='green'> <strong>" ..bd_button.."</strong><br></br></font><strong>"..translate("绑定状态: ").."</strong>".."<font color='green'> <strong>" ..bdzt.."</strong></font></p>")
+--o:depends({oksn="1"})
+
+o = s:option(Flag, "enable", translate("是否启用路由宝"))
+o.rmempty = false
+
+o = s:option(Value, "opsn", translate("SN"),translate("填写自己路由宝背后的SN"))
+--o:depends({oksn="1"})
+o = s:option(ListValue, "wkmod", translate("路由宝工作模式"))
 o:value("0", translate("激进模式"))
 o:value("2", translate("平衡模式"))
 o:value("3", translate("保守模式"))
-pth = s:option(ListValue, "path", translate("缓存文件路径"), translate("请把磁盘挂在到/mnt目录下面"))
-pth:value("", translate("-- Please choose --"))
-local p_user
-for _, p_user in luci.util.vspairs(luci.util.split(luci.sys.exec("df|grep '/mnt/'|awk '{print$6}'"))) do
-	pth:value(p_user)
-end
 
-o = s:option(Value, "pathhc", translate("缓存目录大小限制"), translate("缓存的大小是按1000M=1G算的，如7G的剩余空间就填写7000"))
-o:value("", translate("保持默认"))
-o:value("7000", translate("7G缓存"))
-o:value("14000", translate("14G缓存"))
+
+o = s:option(Value, "pathmeta", translate("meta文件夹目录"), translate("meta文件目录不会占用太多空间"))
+o:value("/mnt/sda1", translate("/mnt/sda1"))
+
+
+o = s:option(Value, "path", translate("缓存目录大小限制"), translate("缓存路径:分配空间，单位M，多个路径用;隔离"))
+o:value("/mnt/sda1/youku:2000", translate("/mnt/sda1/youku:2000"))
+
 
 o = s:option(Value, "cqboot", translate("定时重启"), translate("定时重启，可以自定义重启时间，例：3点重启就输入0300即可，5点半重启就输入0530即可."))
 o:value("", translate("不重启"))
 o:value("0100", translate("1点整重启"))
 o:value("0245", translate("2点45重启"))
 o:value("0300", translate("3点重启"))
-o = s:option(Flag, "ikrebot", translate("只重启矿机"), translate("勾选表示只重启挖矿程序，不勾选则重启路由器。"))
+
+o = s:option(Flag, "ikrebot", translate("只重启路由宝"), translate("勾选表示只重启路由宝程序，不勾选则重启路由器。"))
+
 return m
