2225fcd29138a9b2901231ca827c1d9d61f694da
yetsky <yetsky@gmail.com>
2015-05-23 11:32:49 +0800

    new

--- luci/applications/luci-youku/luasrc/model/cbi/youku.lua
+++ luci/applications/luci-youku/luasrc/model/cbi/youku.lua
@@ -1,42 +1,53 @@
-
 local fs = require "nixio.fs"
+local uci_sn=luci.sys.exec("echo 2115$(cat /sys/class/net/br-lan/address|tr -d ':'|md5sum |tr -dc [0-9]|cut -c 0-12)")
+local yksn=luci.sys.exec("uci get youku.youku.opsn")
+local button = ""
+local sudu = luci.sys.exec("/lib/spd")
+local running = (luci.sys.call("pidof ikuacc > /dev/null") == 0)
+local run = (luci.sys.call("pidof youkudome > /dev/null") == 0)
+local bdsn=luci.sys.exec("getykbdlink 0000$(uci get youku.youku.opsn)|sed -e's/&/&amp;/g'")
+local bdzt=luci.sys.exec("wget -O - http://pcdnapi.youku.com/pcdn/user/check_bindinfo?pid=0000$(uci get youku.youku.opsn)|grep 'name'|cut -d '\"' -f 16")
 
-local youkudome =(luci.sys.call("pidof youkudome > /dev/null") == 0)
-
-if youkudome then	
-	m = Map("youku", translate("youku"), translate("youku is running"))
+bd_button = "<input type=\"button\" value=\" " .. translate("绑定优酷帐号") .. " \" onclick=\"window.open('" .. bdsn .. "')\"/>"
+if running  then
+m = Map("youku", translate("优酷路由宝"), "<p style='text-align:left'>"..translate("路由宝正在工作中....").."<br></br>"..translate("WAN口速率 ")..sudu.."</p>")
 else
-	m = Map("youku", translate("youku"), translate("youku is not running"))
+if run then
+m = Map("youku", translate("优酷路由宝"), translate("路由宝准备工作在...."))
+else
+m = Map("youku", translate("优酷路由宝"), translate("路由宝已停止工作...."))
 end
+end
+s = m:section(TypedSection, "youku", translate("路由宝<a href=\"http://yjb.youku.com\" target=\"_blank\">  点击进入官方金币平台>></a>"))
+s.anonymous = true
+o = s:option(Flag, "enable", translate("是否启用矿机"))
+o = s:option(ListValue, "oksn", translate("SN途径"))
+o:value("1", translate("路由宝原版SN"))
+o = s:option(DummyValue,"","<p style='text-align:left'><strong>"..translate("序列号sn: ").."</strong>".."<font color='green'> <strong>" ..yksn.."</strong><br></br></font><strong>"..translate("一键绑定: ").."</strong>".."<font color='green'> <strong>" ..bd_button.."</strong><br></br></font><strong>"..translate("绑定状态: ").."</strong>".."<font color='green'> <strong>" ..bdzt.."</strong></font></p>")
+o:depends({oksn="1"})
 
-youku = m:section(TypedSection, "youku", translate("Server Setting"))
-youku.anonymous = true
-
-switch = youku:option(Flag, "enable", translate("Enable"))
-switch.rmempty = false
-
-
-password = youku:option(Value, "opsn", translate("SN"))
-password.password = true
-
-remote_server = youku:option(Value, "remote_server", translate("Server Address"))
-remote_server.datatype = ipaddr
-remote_server.optional = false
-
-o = youku:option(ListValue, "wkmod", translate("挖矿模式"))
+o = s:option(Value, "opsn", translate("原版SN"))
+o:depends({oksn="1"})
+o = s:option(ListValue, "wkmod", translate("挖矿模式"))
 o:value("0", translate("激进模式"))
 o:value("2", translate("平衡模式"))
 o:value("3", translate("保守模式"))
+pth = s:option(ListValue, "path", translate("缓存文件路径"), translate("请把磁盘挂在到/mnt目录下面"))
+pth:value("", translate("-- Please choose --"))
+local p_user
+for _, p_user in luci.util.vspairs(luci.util.split(luci.sys.exec("df|grep '/mnt/'|awk '{print$6}'"))) do
+	pth:value(p_user)
+end
 
-o = youku:option(Value, "cqboot", translate("定时重启"), translate("定时重启，可以自定义重启时间，例：3点重启就输入0300即可，5点半重启就输入0530即可."))
+o = s:option(Value, "pathhc", translate("缓存目录大小限制"), translate("缓存的大小是按1000M=1G算的，如7G的剩余空间就填写7000"))
+o:value("", translate("保持默认"))
+o:value("7000", translate("7G缓存"))
+o:value("14000", translate("14G缓存"))
+
+o = s:option(Value, "cqboot", translate("定时重启"), translate("定时重启，可以自定义重启时间，例：3点重启就输入0300即可，5点半重启就输入0530即可."))
 o:value("", translate("不重启"))
 o:value("0100", translate("1点整重启"))
 o:value("0245", translate("2点45重启"))
 o:value("0300", translate("3点重启"))
-
-o = youku:option(Flag, "ikrebot", translate("只重启矿机"), translate("勾选表示只重启挖矿程序，不勾选则重启路由器。"))
-o = false
-
+o = s:option(Flag, "ikrebot", translate("只重启矿机"), translate("勾选表示只重启挖矿程序，不勾选则重启路由器。"))
 return m
-
-
